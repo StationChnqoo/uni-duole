3b1ff0d69452d5a335f7266c7a542e673c244b2a
Station Chenqiao <i@cctv3.net>
2025-08-11 16:44:45 +0800

    feat: 配置pages/TabBar ...

---
+++ src/pages/my/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+	import { ref } from 'vue'
+	import { useCounterStore, useDuoleStore } from '@/stores';
+	const title = ref('Hello')
+	const counterStore = useCounterStore();
+	const duoleStore = useDuoleStore();
+</script>
+
+<template>
+	<view class="content">
+		<text>我的</text>
+	</view>
+</template>
+
+<style scoped lang="scss">
+	.content {
+		display: flex;
+		flex-direction: column;
+		padding: 30rpx;
+		background-color: #f0f0f0;
+		flex: 1;
+		height: 100vh;
+	}
+</style>
